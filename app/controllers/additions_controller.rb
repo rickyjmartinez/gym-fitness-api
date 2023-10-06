e661f68b7b71355decef70d48799213e1a360057
ricky martinez <rickyjmartinez2@gmail.com>
2023-10-06 12:55:31 -0400

    add difficulty as param for get request

--- app/controllers/additions_controller.rb
+++ app/controllers/additions_controller.rb
@@ -1,8 +1,11 @@
 class AdditionsController < ApplicationController
   def index
     muscle = params[:muscle]
+    difficulty = params[:difficulty]
 
-    response = HTTP.headers('X-Api-Key' => "#{Rails.application.credentials.api_ninjas_api_key}").get("https://api.api-ninjas.com/v1/exercises?muscle=#{muscle}")
+    response = HTTP.headers('X-Api-Key' => "#{Rails.application.credentials.api_ninjas_api_key}").get("https://api.api-ninjas.com/v1/exercises?muscle=#{muscle}&difficulty=#{difficulty}")
+
+    # https://api.api-ninjas.com/v1/exercises?muscle=calves&difficulty=beginner
     exercises = response.parse(:json)
     render json: exercises
   end 
